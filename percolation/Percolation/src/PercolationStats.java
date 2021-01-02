3cd0095e733edbd9d0314c130bbf41a3e7c41a30
Garret Bassett <garret.bassett@gmail.com>
2021-01-01 22:15:19 -0700

    restore

---
+++ percolation/Percolation/src/PercolationStats.java
@@ -0,0 +1,96 @@
+import java.util.Random;
+
+public class PercolationStats {
+
+    private int gridSize;
+    private int numTrials;
+    private double[] thresholds;
+
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+
+        if (n <= 0 || trials <= 0) throw new IllegalArgumentException("grid size and number of trials must be greater than zero.");
+
+        this.gridSize = n;
+        this.numTrials = trials;
+        thresholds = new double[trials];
+
+    }
+
+    public void runSim() {
+
+        for (int i = 0; i < numTrials; i++) {
+            System.out.println("SIM #" + (i + 1));
+            // do trial
+            Percolation grid = new Percolation(gridSize);
+            int iterationCount = 0;
+
+            while(!grid.percolates()) {
+//                grid.printGrid();
+//                System.out.println("\n==========\n");
+                Random random = new Random();
+                int row = (int) Math.floor(random.nextInt(gridSize) + 1);
+                int col = (int) Math.floor(random.nextInt(gridSize) + 1);
+//                System.out.println("ROW: " + row + ", COL: " + col);
+                if (!grid.isOpen(row, col)) {
+                    grid.open(row, col);
+                    iterationCount ++;
+                }
+            }
+
+            // store threshold in thresholds array
+            thresholds[i] = (double) iterationCount / (gridSize * gridSize);
+        }
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+
+        double thresholdSum = 0;
+
+        for (double threshold : thresholds) {
+            thresholdSum += threshold;
+        }
+
+        return thresholdSum / numTrials;
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+
+        double stdDev = 0;
+
+        for (double threshold : thresholds) {
+            stdDev += ((threshold - mean()) * (threshold - mean()));
+
+        }
+        stdDev /= numTrials;
+        return Math.sqrt(stdDev);
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo() {
+
+        return (mean() - (1.96 * stddev() / Math.sqrt(numTrials)));
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+
+        return (mean() + (1.96 * stddev() / Math.sqrt(numTrials)));
+    }
+
+    // test client (see below)
+    public static void main(String[] args) {
+
+//        Percolation grid = new Percolation(5);
+        PercolationStats stats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+
+        stats.runSim();
+        for (double threshold : stats.thresholds) System.out.println(threshold);
+
+        System.out.println("AVG: " + stats.mean());
+
+    }
+
+}
